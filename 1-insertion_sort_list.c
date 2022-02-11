f5b9c1d3e6657e76ca03caeb55151a368331bd66
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-02-10 23:41:34 -0500

    first point

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,28 +1,37 @@
 #include "sort.h"
 /**
-*insertion_sort_list - 
-*@list:
+*insertion_sort_list - Ordena los nodos de la lista de menor a mayor
+*@list: Doble lista enlazada a verificar
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *siguiente, *anterior;
+	listint_t *siguiente, *actual;
+	listint_t *anterior;
 
 	if (list == NULL || *list == NULL)
 		return;
-	
-	anterior = (*list)->next;
 
-	while(anterior)
+	actual = (*list)->next;
+
+	while (actual)
 	{
-		siguiente = anterior->next;
-		//verificamos que no sea nulo y que el valor sea mayor a n
-		while(anterior->prev != NULL && anterior->prev->n > anterior->n)
+		siguiente = actual->next;
+		anterior = actual->prev;
+		while (anterior != NULL && actual->n < anterior->n)
 		{
-			if(anterior->next != NULL)
-				anterior->next->prev = anterior->prev;
-			anterior->next = anterior->prev;
-			anterior->prev = anterior->next->prev;
-			anterior->next->prev = anterior;
+			anterior->next = actual->next;
+			if (actual->next != NULL)
+				actual->next->prev = anterior;
+			actual->prev = anterior->prev;
+			actual->next = anterior;
+			if (anterior->prev != NULL)
+				anterior->prev->next = actual;
+			else
+				*list = actual;
+			anterior->prev = actual;
+			anterior = actual->prev;
+			print_list(*list);
 		}
+		actual = siguiente;
 	}
 }
